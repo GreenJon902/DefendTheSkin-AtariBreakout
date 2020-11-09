5b80408142ff667c9196169052d100e4a25cbf75
jj b <jj@thebabers.at>
2020-11-09 15:51:17 +0000

    better hart sizing

--- health.py
+++ health.py
@@ -16,18 +16,18 @@ class Health(Widget):
         self.bind(health=self.update)
 
     def update(self, _=None, _2=None, _3=None):
-        self.ids["heart_1"].size = self.parent.width * heartSize, self.parent.width * heartSize
-        self.ids["heart_2"].size = self.parent.width * heartSize, self.parent.width * heartSize
-        self.ids["heart_3"].size = self.parent.width * heartSize, self.parent.width * heartSize
+        self.ids["heart_1"].size = self.parent.height * heartSize, self.parent.height * heartSize
+        self.ids["heart_2"].size = self.parent.height * heartSize, self.parent.height * heartSize
+        self.ids["heart_3"].size = self.parent.height * heartSize, self.parent.height * heartSize
 
-        self.ids["heart_1"].right = self.parent.width - (self.parent.width * healthDistance)
+        self.ids["heart_1"].right = self.parent.width - (self.parent.height * healthDistance)
         self.ids["heart_1"].top = self.parent.height - (self.parent.height * healthDistance)
 
-        self.ids["heart_2"].right = self.parent.width - (self.parent.width * heartSize) - (
+        self.ids["heart_2"].right = self.parent.width - (self.parent.height * heartSize) - (
                     self.parent.width * healthDistance * 2)
         self.ids["heart_2"].top = self.parent.height - (self.parent.height * healthDistance)
 
-        self.ids["heart_3"].right = self.parent.width - (self.parent.width * heartSize * 2) - (
+        self.ids["heart_3"].right = self.parent.width - (self.parent.height * heartSize * 2) - (
                     self.parent.width * healthDistance * 3)
         self.ids["heart_3"].top = self.parent.height - (self.parent.height * healthDistance)
 
